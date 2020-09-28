03d4d333f9ac7b014c433d9859f8ac150cca6744
Nuage <lenuagebrun@gmail.com>
2020-09-28 10:22:46 -0400

    first push

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = {
+    term: ''
+  }
+  onFormSubmit = e => {
+    e.preventDefault()
+    this.props.onSubmit(this.state.term);
+  }
+  // Currently the class names are tied to CDN styling from Semantic UI. If you don't want it remove the class name properties.
+  render() {
+    return (
+      <div id='search-bar'>
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <input 
+            placeholder='Enter address here...'
+            type="text" 
+            value={this.state.term}
+            onChange={(e) => this.setState({term: e.target.value })} />
+          </div>
+        </form>
+      </div>
+    )
+  };
+}
+export default SearchBar
